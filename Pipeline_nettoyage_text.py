2509c36bf698a79e53be31d222146f023c145856
Aurelien Kuleczka <aurelienk64@gmail.com>
2021-05-07 14:39:10 +0200

    Pipeline nettoyant un texte

---
+++ Pipeline_nettoyage_text.py
@@ -0,0 +1,31 @@
+#Tokenization d'une phrase
+
+from textblob import TextBlob
+import re
+
+
+text_doliprane = TextBlob("The DOLIPRANE //       is a good drug when "
+                          ":;!  you have some fever 12")
+
+def pipeline(text):
+
+    text = text.lower()
+    text = text.replace('\n', ' ').replace('\r', '')
+    text = ' '.join(text.split())
+    text = re.sub(r"[A-Za-z\.]*[0-9]+[A-Za-z%°\.]*", "", text)
+    text = re.sub(r"(\s\-\s|-$)", "", text)
+    text = re.sub(r"[,\;\!\?\%\(\)\/\"]", "", text)
+    text = re.sub(r"\&\S*\s", "", text)
+    text = re.sub(r"\&", "", text)
+    text = re.sub(r"\+", "", text)
+    text = re.sub(r"\#", "", text)
+    text = re.sub(r"\$", "", text)
+    text = re.sub(r"\£", "", text)
+    text = re.sub(r"\%", "", text)
+    text = re.sub(r"\:", "", text)
+    text = re.sub(r"\@", "", text)
+    text = re.sub(r"\-", "", text)
+
+    return text
+
+print(pipeline(text_doliprane))
